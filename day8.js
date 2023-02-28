d760921bbd67a623a7f9ad827e36f449a87e71b4
Talib <mdtalib065@gmail.com>
2023-02-28 17:21:21 +0530

    display system

---
+++ day8.js
@@ -0,0 +1,5 @@
+var os=require('os');
+console.log('OS Type:' +os.type());
+console.log('Platform:' +os.platform());
+console.log('Total memory:' +os.totalmem()+'bytes');
+console.log('Free memory:' +os.freemem()+'bytes');
