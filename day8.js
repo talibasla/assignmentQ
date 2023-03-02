bf7dcc5fb04280d011512f7c06d4dc904295d572
talibasla <97939454+talibasla@users.noreply.github.com>
2023-03-02 19:26:50 +0530

    Delete day8.js

--- day8.js
+++
@@ -1,5 +0,0 @@
-var os=require('os');
-console.log('OS Type:' +os.type());
-console.log('Platform:' +os.platform());
-console.log('Total memory:' +os.totalmem()+'bytes');
-console.log('Free memory:' +os.freemem()+'bytes');
