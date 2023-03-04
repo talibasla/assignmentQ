d34366ffbcbfdd42d68db4c0e90b680e69f2a9f3
Talib <mdtalib065@gmail.com>
2023-03-05 02:50:16 +0530

    Comment Remove

--- day8.js
+++ day8.js
@@ -1,6 +1,12 @@
 //dispay system information
-var os=require('os');
-console.log('OS Type:' +os.type());
-console.log('Platform:' +os.platform());
-console.log('Total memory:' +os.totalmem()+'bytes');
-console.log('Free memory:' +os.freemem()+'bytes');
+var os = require('os');
+//This is for os type
+console.log('OS Type:' + os.type());
+ 
+console.log('Platform:' + os.platform());
+ 
+console.log('Total Memory:' + os.totalmem() + 'bytes');
+//This is Host name
+console.log('Host Name:' + os.hostname)
+//This is for Free memory
+console.log('Free Memory:' + os.freemem() + 'bytes');
